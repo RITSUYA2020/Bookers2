2198790411935b9b755cd9104723c5be3dd5f651
RITSUYA2020 <12ritsuya23@gmail.com>
2020-05-28 18:36:13 +0900

    [Finish]5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+
+before_action :ensure_current_user,{only: [:edit, :update, :destroy]}
+
+	def create
+  		@book = Book.new(book_params)
+  		@book.user_id = current_user.id
+  		if @book.save
+  			flash[:notice] = "You have creatad book successfully."
+  			redirect_to book_path(@book.id)
+  		else
+  			#indexページの表示処理
+  			@books = Book.all
+  			@user = @book.user
+  			render :index
+  		end
+  	end
+
+	#indexページの表示処理
+  	def index
+		@books = Book.all
+		@new_book = Book.new
+		@user = current_user
+	end
+
+	def show
+		@book = Book.find(params[:id])
+		@user = @book.user
+		@new_book = Book.new
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:notice] = "Book was successfully created."
+			redirect_to book_path(@book.id)
+		else
+			@user = @book.user
+			render :show
+		end
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+	def ensure_current_user
+		@book = Book.find_by(id: params[:id])
+		if current_user.id != @book.user_id
+			redirect_to books_path
+		end
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_115241) do
+ActiveRecord::Schema.define(version: 2020_05_24_052736) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
